b8a409987caf29163a5513d60b585bd6f2e2f9f2
Michaella Babbitt <tinydynamine@gmail.com>
2020-02-26 18:42:23 -0600

    troubleshooting troubleshot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule, 
+    AppRoutingModule, 
+    ReactiveFormsModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
